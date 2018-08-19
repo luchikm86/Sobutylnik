47a92ac7f462755326392d361ad284908362fe87
luchikm86 <luchik.m86@gmail.com>
2018-08-19 14:32:18 +0300

    SBT-2

--- src/main/java/com/sobutylnik/JavaConfig/ApplicationConfig.java
+++ src/main/java/com/sobutylnik/JavaConfig/ApplicationConfig.java
@@ -1,25 +1,17 @@
 package com.sobutylnik.JavaConfig;
 
-import org.springframework.aop.interceptor.AbstractMonitoringInterceptor;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
-import com.sobutylnik.hello.IndexController;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.sobutylnik" })
 public class ApplicationConfig {
 
-    private AbstractMonitoringInterceptor resolver;
-
-    @Bean
-    public IndexController indexController() {
-        return new IndexController();
-    }
 
 
 }
--- src/main/java/com/sobutylnik/hello/IndexController.java
+++ src/main/java/com/sobutylnik/hello/IndexController.java
@@ -1,18 +1,31 @@
 package com.sobutylnik.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 
-@Controller
-public class IndexController {
 
-    @RequestMapping ( "/index" )
-    public ModelAndView helloWorld () {
-        ModelAndView mav = new ModelAndView ();
-        mav.setViewName ( "helloWorld" );
-        mav.addObject ("Hello World!");
-        return mav;
+    @Controller
+    public class IndexController {
+
+    @ResponseBody
+    public String HelloWorld() {
+        return "hello";
     }
 }
+
+//@Controller
+//public class IndexController {
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+//    public String printHello(ModelMap model) {
+//        model.addAttribute("message", "Hello Spring MVC Framework!");
+//        return "hello";
+//    }
+//}
+
