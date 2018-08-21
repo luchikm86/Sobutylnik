c408e0dd7e59499f5dcbc638d3a2c97cfc757e98
luchikm86 <luchik.m86@gmail.com>
2018-08-21 15:24:49 +0300

    SBT-2 attempt 1.4

--- src/main/java/com/sobutylnik/hello/IndexController.java
+++ src/main/java/com/sobutylnik/hello/IndexController.java
@@ -1,16 +1,14 @@
 package com.sobutylnik.hello;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 
 @Controller
     public class IndexController {
 
-        @RequestMapping(value="/index", method=GET)
         @ResponseBody
         public String textHello() {
             return "Hello, Sobutylnik!";
