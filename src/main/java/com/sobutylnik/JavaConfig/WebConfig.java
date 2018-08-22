54241dd4d2b6303c0dd9d2044c3072ecf5c3fe29
luchikm86 <luchik.m86@gmail.com>
2018-08-22 09:54:26 +0300

    SBT-2 - 1.6

--- src/main/java/com/sobutylnik/JavaConfig/WebConfig.java
+++ src/main/java/com/sobutylnik/JavaConfig/WebConfig.java
@@ -1,49 +1,49 @@
-package com.sobutylnik.JavaConfig;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-
-@Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = { "com.sobutylnik" })
-public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-    public UrlBasedViewResolver setupViewResolver() {
-        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-        resolver.setPrefix("/WEB-INF/pages/");
-        resolver.setSuffix(".jsp");
-        resolver.setViewClass(JstlView.class);
-        return resolver;
-    }
-
-    @Bean
-    public CommonsMultipartResolver multipartResolver() {
-        return new CommonsMultipartResolver();
-    }
-
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.enableContentNegotiation(new MappingJackson2JsonView());
-        registry.jsp();
-    }
-
-}
+//package com.sobutylnik.JavaConfig;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+//import org.springframework.web.servlet.config.annotation.*;
+//import org.springframework.web.servlet.view.JstlView;
+//import org.springframework.web.servlet.view.UrlBasedViewResolver;
+//import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+//
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan(basePackages = { "com.sobutylnik" })
+//public class WebConfig implements WebMvcConfigurer {
+//
+////    @Bean
+////    public UrlBasedViewResolver setupViewResolver() {
+////        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+////        resolver.setPrefix("/WEB-INF/pages/");
+////        resolver.setSuffix(".jsp");
+////        resolver.setViewClass(JstlView.class);
+////        return resolver;
+////    }
+//
+//    @Bean
+//    public CommonsMultipartResolver multipartResolver() {
+//        return new CommonsMultipartResolver();
+//    }
+//
+//
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
+//
+////    @Override
+////    public void addViewControllers(ViewControllerRegistry registry) {
+////        registry.addViewController("/").setViewName("home");
+////    }
+//
+//    @Override
+//    public void configureViewResolvers(ViewResolverRegistry registry) {
+//        registry.enableContentNegotiation(new MappingJackson2JsonView());
+//        registry.jsp();
+//    }
+//
+//}
--- src/main/java/com/sobutylnik/hello/IndexController.java
+++ src/main/java/com/sobutylnik/hello/IndexController.java
@@ -1,17 +1,22 @@
 package com.sobutylnik.hello;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-
-@Controller
+@RestController
     public class IndexController {
 
-        @ResponseBody
-        public String textHello() {
-            return "Hello";
-        }
+//        @ResponseBody
+//        public String textHello() {
+//            return "Hello";
+//        }
+
+    @RequestMapping(value = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String textHello() { return "Hello"; }
 }
 
