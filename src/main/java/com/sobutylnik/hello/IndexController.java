9cad62aee9253296661c8bbafce6d9c4ca6710fd
luchikm86 <luchik.m86@gmail.com>
2018-08-23 19:31:41 +0300

    SBT-2 - 1.8

--- src/main/java/com/sobutylnik/hello/IndexController.java
+++ src/main/java/com/sobutylnik/hello/IndexController.java
@@ -1,7 +1,5 @@
 package com.sobutylnik.hello;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
