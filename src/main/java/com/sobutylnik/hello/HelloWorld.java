c08164f6894fe2f68d6c0d0afe93b36480ed8155
luchikm86 <luchik.m86@gmail.com>
2018-08-23 19:29:57 +0300

    SBT-2 - 1.8

--- src/main/java/com/sobutylnik/hello/HelloWorld.java
+++ src/main/java/com/sobutylnik/hello/HelloWorld.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorld {
     public static void main(String[] args) {
-        
+
 //        Runnable helloPrinter = () -> System.out.println("Hello");
 //        helloPrinter.run();
 
--- src/main/java/com/sobutylnik/hello/IndexController.java
+++ src/main/java/com/sobutylnik/hello/IndexController.java
@@ -1,44 +1,17 @@
 package com.sobutylnik.hello;
 
-//import org.springframework.http.MediaType;
-//import org.springframework.stereotype.Controller;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//
-//@RestController
-//    public class IndexController {
-//
-////        @ResponseBody
-////        public String textHello() {
-////            return "Hello";
-////        }
-//
-//    @RequestMapping(value = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public String textHello() { return "json"; }
-//
-////    @RequestMapping(value = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
-////    public String textHello() { return "Hello"; }
-//}
-
-
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
     private static final String hello = "Hello";
-    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/")
 //    @RequestMapping(value = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
     public String index(){
-        return "dfsdfsdf";
+        return hello;
     }
 }
