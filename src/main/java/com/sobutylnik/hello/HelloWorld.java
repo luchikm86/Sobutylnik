8e92ddff235eb680a2df90c008e3f622ed4cfde3
luchikm86 <luchik.m86@gmail.com>
2018-08-23 19:28:36 +0300

    SBT-2 - 1.7

--- src/main/java/com/sobutylnik/hello/HelloWorld.java
+++ src/main/java/com/sobutylnik/hello/HelloWorld.java
@@ -1,8 +1,15 @@
 package com.sobutylnik.hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HelloWorld {
     public static void main(String[] args) {
-        Runnable helloPrinter = () -> System.out.println("Hello");
-        helloPrinter.run();
+        
+//        Runnable helloPrinter = () -> System.out.println("Hello");
+//        helloPrinter.run();
+
+        SpringApplication.run(HelloWorld.class, args);
     }
 }
--- src/main/java/com/sobutylnik/hello/IndexController.java
+++ src/main/java/com/sobutylnik/hello/IndexController.java
@@ -1,22 +1,44 @@
 package com.sobutylnik.hello;
 
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+//import org.springframework.http.MediaType;
+//import org.springframework.stereotype.Controller;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//@RestController
+//    public class IndexController {
+//
+////        @ResponseBody
+////        public String textHello() {
+////            return "Hello";
+////        }
+//
+//    @RequestMapping(value = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public String textHello() { return "json"; }
+//
+////    @RequestMapping(value = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
+////    public String textHello() { return "Hello"; }
+//}
 
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-    public class IndexController {
+public class IndexController {
+    private static final String hello = "Hello";
+    private final AtomicLong counter = new AtomicLong();
 
-//        @ResponseBody
-//        public String textHello() {
-//            return "Hello";
-//        }
-
-    @RequestMapping(value = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String textHello() { return "Hello"; }
+    @RequestMapping("/")
+//    @RequestMapping(value = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String index(){
+        return "dfsdfsdf";
+    }
 }
-
