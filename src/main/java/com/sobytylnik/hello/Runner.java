e9d73d4e33d6a9a6ef8534e2fbe6616e1c29cd41
Arenius <apalko@online.ua>
2018-09-02 19:37:54 +0300

    1

---
+++ src/main/java/com/sobytylnik/hello/Runner.java
@@ -0,0 +1,21 @@
+package com.sobytylnik.hello;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Runner {
+    public static void main(String[] args) {
+        ConcurrentHashMap b = new ConcurrentHashMap();
+        Profile friend = new Profile(12L, "Sergey", "Apalko", 29);
+        Profile friend1 = new Profile(13L, "Max", "Luchenko", 29);
+        Profile friend2 = new Profile(14L, "General", "Panama", 29);
+        System.out.println(friend);
+        ConcurrentHashMap friendList = new ConcurrentHashMap();
+        InMemoryProfileRepository test = new InMemoryProfileRepository(friendList);
+        test.save(friend);
+
+        System.out.println(test);
+        friend.changeSurName("Random");
+        test.merge(friend);
+        System.out.println(test);
+    }
+}
