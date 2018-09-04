d02ba1fe66bcab1f15063e65cf244e9139b53aae
Kurenkov27 <36358857+Kurenkov27@users.noreply.github.com>
2018-09-04 20:52:09 +0300

    SBT-4-Creating several tests

--- src/main/java/com/sobytylnik/hello/MyTest.java
+++ src/main/java/com/sobytylnik/hello/MyTest.java
@@ -11,24 +11,46 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MyTest {
     Profile friend = new Profile(12L, "Sergey", "Apalko", 29);
     Profile friend1 = new Profile(13L, "Max", "Luchenko", 29);
-    Profile friend2 = new Profile(12L, "General", "Panama", 29);
+    Profile friend2 = new Profile(14L, "General", "Panama", 29);
+    Profile checkProfile = new Profile(14L,"Kolyamba","Kurenkov",27);
+    InMemoryProfileRepository testReposit = new InMemoryProfileRepository(new ConcurrentHashMap<>());
 
-    ConcurrentHashMap friendList = new ConcurrentHashMap();
-    InMemoryProfileRepository testReposit = new InMemoryProfileRepository(friendList);
+    
+    @Test
+    public void testFindById(){
+        testReposit.save(friend2);
 
-
-
+        assertTrue(testReposit.findById(22L).equals(friend2));
+    }
 
     @Test
-public void testRepository(){
-testReposit.save(friend);
+    public void testSave(){
+        testReposit.save(friend);
+        testReposit.save(friend1);
+        assertEquals(testReposit.findById(13L), friend1);
+    }
 
-
-        assertEquals(Optional.of(friend), testReposit.findById(12L));
-        assertEquals(friend1, testReposit.save(friend1));
-        
-
-
+    @Test(expected = IllegalArgumentException.class)
+    public void testSave2(){
+        testReposit.save(friend1);
+        testReposit.save(friend1);
 
     }
+
+    @Test
+    public void testMerge(){
+
+        testReposit.save(friend2);
+        testReposit.merge(checkProfile);
+
+        assertEquals(testReposit.findById(14L),Optional.of(checkProfile));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMerge2(){
+        Profile checkProfile2 = new Profile(145L,"Kolyamba","Kurenkov",27);
+        testReposit.save(friend2);
+        testReposit.merge(checkProfile2);
+    }
+
 }
