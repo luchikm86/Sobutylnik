d057d83f779d0d8cbabb36d02754670284209851
Nikolay <kurenkov.mykola@ukr.net>
2019-01-30 16:32:55 +0200

    API documentation with Swagger

--- src/main/java/com/sobytylnik/ProfileController.java
+++ src/main/java/com/sobytylnik/ProfileController.java
@@ -1,5 +1,6 @@
 package com.sobytylnik;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
