687e15ad8b8bfdbaef49198320b08b666ac1ab37
luchikm86 <luchik.m86@gmail.com>
2019-02-25 03:19:17 +0200

    SBT-18-3

--- sql/fill-db.sql
+++ sql/fill-db.sql
@@ -1,3 +1,3 @@
-INSERT INTO profiles (name, surname, age, location, language, birthDate, twitterPage, instagramPage, facebookPage, telegramUserName) 
+INSERT INTO profiles (name, surname, age, location, language, birthDate, twitterPage, instagramPage, facebookPage, telegramUserName)
 VALUES('James', 'Bond', 40, 'Kiev, Ukraine', 23011987),
 ('Albert', 'Einstein', 66, 'Vinitsa', 03121997),('Victor', 'Gugo', 22, 'Lviv', 16091977);
